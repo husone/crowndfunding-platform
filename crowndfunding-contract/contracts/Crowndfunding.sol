908bec5fdb5976c8543cbe919f45dc1e44ea7dde
husone <sonhvnde160056@fpt.edu.vn>
2023-05-26 14:53:31 +0700

    smart contract done

--- crowndfunding-contract/contracts/Crowndfunding.sol
+++ crowndfunding-contract/contracts/Crowndfunding.sol
@@ -11,12 +11,18 @@ contract Crownfunding {
         uint256 raised;
         string image;
         address[] contributors;
-        mapping(address => uint256) contributions;  
-    } 
-
+    }
+    mapping(address => mapping(uint256 => uint256)) public contributions;
     mapping(uint256 => Campaign) public campaigns;
     uint256 public campaignCount = 0;
-    function createCampaign(string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+
+    function createCampaign(
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
         Campaign storage campaign = campaigns[campaignCount];
         require(_deadline > block.timestamp, "Deadline must be in the future");
         campaign.owner = msg.sender;
@@ -32,9 +38,80 @@ contract Crownfunding {
     function contribute(uint256 _campaignId) public payable {
         Campaign storage campaign = campaigns[_campaignId];
         require(block.timestamp < campaign.deadline, "Campaign is closed");
-        campaign.contributions[msg.sender] += msg.value;
+        (bool sent, ) = payable(campaign.owner).call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+        contributions[msg.sender][_campaignId] += msg.value;
         campaign.raised += msg.value;
+        require(
+            campaign.raised <= campaign.target,
+            "Campaign is reached the target"
+        );
+        require(campaign.raised <= campaign.target, "Campaign is closed");
+        for(uint i = 0; i< campaign.contributors.length; i++) {
+            if(campaign.contributors[i] == msg.sender) {
+                return;
+            }
+        }
         campaign.contributors.push(msg.sender);
     }
 
+    function getCampaignContributors(
+        uint256 _campaignId
+    ) public view returns (address[] memory, uint256[] memory) {
+        Campaign storage campaign = campaigns[_campaignId];
+        uint256[] memory contributionsList = new uint256[](
+            campaign.contributors.length
+        );
+        for (uint256 i = 0; i < campaign.contributors.length; i++) {
+            contributionsList[i] = contributions[campaign.contributors[i]][_campaignId];
+        }
+        return (campaign.contributors, contributionsList);
+    }
+
+    function getCampaignContribution(
+        uint256 _campaignId
+    , address caller) public view returns (uint256) {
+        Campaign storage campaign = campaigns[_campaignId];
+        return contributions[caller][_campaignId];
+    }
+
+    function getCampaign(
+        uint256 _campaignId
+    )
+        public
+        view
+        returns (
+            address,
+            string memory,
+            string memory,
+            uint256,
+            uint256,
+            uint256,
+            string memory
+        )
+    {
+        Campaign storage campaign = campaigns[_campaignId];
+        return (
+            campaign.owner,
+            campaign.title,
+            campaign.description,
+            campaign.target,
+            campaign.deadline,
+            campaign.raised,
+            campaign.image
+        );
+    }
+
+    function getCampaignCount() public view returns (uint256) {
+        return campaignCount;
+    }
+
+    function getAllCampaigns() public view returns (Campaign[] memory){
+        Campaign[] memory _campaigns = new Campaign[](campaignCount);
+        for (uint i = 0; i < campaignCount; i++) {
+            Campaign storage campaign = campaigns[i];
+            _campaigns[i] = campaign;
+        }
+        return _campaigns;
+    }
 }
--- crowndfunding-contract/hardhat.config.js
+++ crowndfunding-contract/hardhat.config.js
@@ -1,4 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+
 module.exports = {
   solidity: {
     version: '0.8.9',
@@ -9,4 +11,13 @@ module.exports = {
       },
     },
   },
+  networks: {
+    bscTestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+  },
+  
 };
